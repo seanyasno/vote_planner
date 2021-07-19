7897ffe8c85beaad3fe561bffdf69eb5f91aa8ff
seanyasno <sean.yasno@gmail.com>
2021-07-19 19:04:27 +0300

    little fix

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -22,90 +22,95 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome',
-              style: TextStyle(
-                fontWeight: Theme.of(context).textTheme.headline3?.fontWeight,
-                fontSize: Theme.of(context).textTheme.headline3?.fontSize,
-                color: Theme.of(context).primaryColorLight,
-              ),
-            ),
-            SizedBox(height: 160),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 80),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Enter your name here',
-                        hintStyle: TextStyle(
-                          color: Theme.of(context).primaryColorDark,
-                        ),
-                        errorStyle: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                        errorBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                        focusedErrorBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      maxLength: 24,
-                      minLines: 1,
-                      maxLines: 1,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Your name cannot be empty';
-                        }
-
-                        return null;
-                      },
-                      controller: _textEditingController,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Theme.of(context).primaryColorDark,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          try {
-                            final User newUser =
-                                await UserController.insertUser(
-                                    name: _textEditingController.text);
-                            await userProvider.setUser(user: newUser);
-                            Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                builder: (context) => PlannersPage(),
-                              ),
-                            );
-                          } catch (error) {}
-                        }
-                      },
-                      child: Text('Continue'.toUpperCase()),
-                    ),
-                  ],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontWeight:
+                        Theme.of(context).textTheme.headline3?.fontWeight,
+                    fontSize: Theme.of(context).textTheme.headline3?.fontSize,
+                    color: Theme.of(context).primaryColorLight,
+                  ),
                 ),
-              ),
+                SizedBox(height: 160),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 80),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter your name here',
+                            hintStyle: TextStyle(
+                              color: Theme.of(context).primaryColorDark,
+                            ),
+                            errorStyle: TextStyle(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                            errorBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                            focusedErrorBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                          ),
+                          style: TextStyle(color: Colors.white),
+                          maxLength: 24,
+                          minLines: 1,
+                          maxLines: 1,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Your name cannot be empty';
+                            }
+
+                            return null;
+                          },
+                          controller: _textEditingController,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Theme.of(context).primaryColorDark,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              try {
+                                final User newUser =
+                                    await UserController.insertUser(
+                                        name: _textEditingController.text);
+                                await userProvider.setUser(user: newUser);
+                                Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                    builder: (context) => PlannersPage(),
+                                  ),
+                                );
+                              } catch (error) {}
+                            }
+                          },
+                          child: Text('Continue'.toUpperCase()),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
